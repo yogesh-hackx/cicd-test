cab343abb9987477db0ea59c1842756e51efae4d
Yogesh Bhatt <me@yogicodes.com>
2023-04-13 22:59:58 +0530

    update deploy.sh

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -1,6 +1,8 @@
 # if this is auto-scaling-pipeline, find ip addresses of all the instances present in the autoscaling group, else deploy to the single prod-server
 # send build data to s3 excluding the node-modules, .git
 
+sudo pip install awscli
+
 aws s3 sync . s3://pvhubdeployments/pay-ally --exclude ".git/*" --exclude "node_modules/*"
 
 ssh -oStrictHostKeyChecking=no -v ec2-user@43.205.240.113 "./deploy.sh"
